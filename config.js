e5da984b160bdfc7fad57d6576a151bf1449472b
Emre Aşcı <106581002+emrsc1@users.noreply.github.com>
2023-10-16 11:06:31 +0300

    done

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        port:3310,
+        password: "Emremre5234",
+        database: "blogapp"
+    }
+}
+
+module.exports = config;
